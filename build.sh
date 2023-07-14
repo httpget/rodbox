4826b7511f89385ef5708608f0eab5effe23d07e
httpget <embers11gg@gmail.com>
2023-07-14 19:33:20 -0400

    more changes!!

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ echo replacing __MAIN_SCRIPT
 node safe.js
 echo compiling
 g++ ./tmp/modified.cpp -o ./bin/rodbox -llua5.3 -I/usr/include/lua5.3
-# rm -rf ./tmp/modified.cpp
+rm -rf ./tmp/modified.cpp
